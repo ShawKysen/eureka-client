ce96b0bb330047b16fba23a941fa318f80696ef5
xiaopeng <xiaopeng@ouchteam.com>
2018-10-23 17:32:34 +0800

    init this project

---
+++ src/main/java/org/kysen/eurekaclient/controller/UserController.java
@@ -0,0 +1,29 @@
+package org.kysen.eurekaclient.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String index() {
+		List<ServiceInstance> instances = client.getInstances("hello-service");
+		for (int i = 0; i < instances.size(); i++) {
+			logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+		}
+		return "Hello World";
+	}
+}
